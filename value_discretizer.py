f0d7eb4759ece55f6b68854301b122cb366bb5a3
Ramkumar Hariharan <rmhariharan@users.noreply.github.com>
2016-05-16 20:20:44 -0700

    Update value_discretizer.py

--- value_discretizer.py
+++ value_discretizer.py
@@ -13,38 +13,38 @@ import pandas as p
 File = p.read_csv(file_path,index_col = 0)
 
 if datatype == 'Microarray data':
-    Means = File.mean()
-    Stds = File.std()
+    means = File.mean()
+    stds = File.std()
     stddev = float(easygui.enterbox("Enter a value for the number of standard deviations from mean: ","Standard Deviation factor"))
-    listMeans = list()
-    listStds = list()
+    listmeans = list()
+    liststds = list()
 
-    for i in Means:
-        listMeans.append(i)
-    for i in Stds:
-        listStds.append(i)
+    for i in means:
+        listmeans.append(i)
+    for i in stds:
+        liststds.append(i)
     x =0
     listoflowers = list()
     ## Make list of lowerthresholds for each sample by subtracting half a standard deviation from the mean for each sample
-    for i in listMeans:
-        lower = i-(stddev*listStds[x])
+    for i in listmeans:
+        lower = i-(stddev*liststds[x])
         x = x+1
         listoflowers.append(lower)
     ## Make list of upperthresholds for each sample by adding half a standard deviation from the mean for each sample. Since x has already been used, use new variable y
 
     listofuppers = list()
     y = 0
-    for number in listMeans:
-        upper = number+(stddev*listStds[y])
+    for number in listmeans:
+        upper = number+(stddev*liststds[y])
         y = y+1
         listofuppers.append(upper)
 
     columnname = list()
     m = 0
     z = 0
-    Biggerlist =  list()
-    for columns in File:
-        s = File.take([z],axis = 1)
+    biggerlist =  list()
+    for columns in file:
+        s = file.take([z],axis = 1)
         listofvalues = s.values
         ##print (listofvalues)
         z = z+1
@@ -56,15 +56,15 @@ if datatype == 'Microarray data':
                 columnname.append(2)
             else:
                 columnname.append(1)
-        Biggerlist.append(columnname)
+        biggerlist.append(columnname)
         columnname = list()
-    headers = list(File.columns.values)
-    Rownames = list(File.index)
-    Finaltable = p.DataFrame(Biggerlist)
-    Finaltable = Finaltable.transpose()
+    headers = list(file.columns.values)
+    rownames = list(file.index)
+    finaltable = p.DataFrame(biggerlist)
+    finaltable = finaltable.transpose()
 
-    Finaltable.index = Rownames
-    Finaltable.columns = headers
+    finaltable.index = rownames
+    finaltable.columns = headers
 
 ## For RNAseq input data
 
@@ -72,14 +72,14 @@ elif datatype == 'RNAseq data':
     columnname = list()
     m = 0
     z = 0
-    Biggerlist =  list()
+    biggerlist =  list()
     lower = float(easygui.enterbox("Enter a value for the number of FPKMs you would like to set as lower threshold: ","FPKMs"))
     percentilevalue = float(easygui.enterbox("Enter a value for the percentile you would like to set as upper threshold: ","FPKMs"))
-    newstat = File.describe(percentiles = percentilevalue)
+    newstat = file.describe(percentiles = percentilevalue)
     n =0
     valuelist = list()
-    for columns in File:
-        s = File.take([z],axis = 1)
+    for columns in file:
+        s = file.take([z],axis = 1)
         listofvalues = s.values
         ##print (listofvalues)
         z = z+1
@@ -95,21 +95,21 @@ elif datatype == 'RNAseq data':
             else:
                 columnname.append(0)
         n = n+1
-        Biggerlist.append(columnname)
+        biggerlist.append(columnname)
         columnname = list()
-    headers = list(File.columns.values)
-    Rownames = list(File.index)
-    Finaltable = p.DataFrame(Biggerlist)
-    Finaltable = Finaltable.transpose()
+    headers = list(file.columns.values)
+    rownames = list(file.index)
+    finaltable = p.DataFrame(biggerlist)
+    finaltable = finaltable.transpose()
 
-    Finaltable.index = Rownames
-    Finaltable.columns = headers
+    finaltable.index = rownames
+    finaltable.columns = headers
 ##print (Finaltable)
 
 ##write out as csv
 ##Finaltable.to_csv('C:/Users/rharihar/Desktop/Exercises/testdiscrete1.csv',sep = ',',header = True,index= True,mode= 'w')
 
-Finaltable.to_csv(filedialog.asksaveasfile())
+finaltable.to_csv(filedialog.asksaveasfile())
 
 
 
